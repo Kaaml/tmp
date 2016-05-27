b65353c9eb333d48b7d9207b2c27d51a465b2097
Kaaml <unamed@gmail.com>
2016-05-27 22:31:52 +0200

    working: MSG, JOIN, NICK

--- serverSide/src/ClientThread.java
+++ serverSide/src/ClientThread.java
@@ -27,13 +27,16 @@ public class ClientThread extends Thread {
             e.printStackTrace();
         }
     }
+    String getUserName(){
+        return this.userName;
+    }
     @Override
     public void run(){
         String msg = null;
         try {
             while ((msg = in.readLine()) != null) {
                 System.out.println(socket.getInetAddress() + " MSG{'" + msg + "'}");
-                server.sendToAll(msg );
+                //server.sendToAll(msg );
                 handleMessage(msg);
             }
         }catch (SocketException e ){
@@ -47,18 +50,36 @@ public class ClientThread extends Thread {
     public void send( String msg){
         outWritter.println( msg );
     }
+    public void send( String msg, String from ){
+        outWritter.println( "MSG " + from + " " + msg );
+    }
 
     public void handleMessage( String msg ){
         String command = msg.split(" ", 2 )[0];
-        switch ( command){
+        switch ( command.toUpperCase() ){
             case "JOIN":
                 server.handleJoin( this, msg );
                 break;
+            case "NICK" :
+                this.handleNick( msg );
+                break;
+            case "MSG":
+                server.handleMsg(this, msg );
+                break;
+            case "USERS":
+                server.handleUsers(this, msg );
+                break;
             default:
                 System.out.println("Command not found" );
+                System.out.println( "Command[ " + command + " ] " );
                 //send error or sth
                 break;
         }
 
+    }
+    private void handleNick( String msg ){
+        String nick = msg.split( " ", 2 )[1];
+        this.userName = nick;
+        server.handleNick( this, msg );
     }
 }
--- serverSide/src/Server.java
+++ serverSide/src/Server.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.SessionState;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
@@ -8,13 +10,15 @@ import java.util.Set;
  * Created by kaaml on 16.05.16.
  */
 public class Server {
-
-    private Set< ClientThread> userts = new HashSet<>();
+    private Set<ClientThread> connections = new HashSet<>();
+    private Set< ClientThread> registerUser = new HashSet<>();
     private HashMap<String, Channel> serverChannels = new HashMap<>();
 
 
 
+
     class Channel{
+
         protected String channelTopic = "";
         protected String chanelName;
 
@@ -27,12 +31,22 @@ public class Server {
         }
 
         public void addMember( ClientThread newMember ){
-            this.send( "New member has joined to channel " );
+            this.send( "Member " + newMember.getUserName() + " has joined to channel " );
             channelMembers.add( newMember );
         }
         public void send( String msg ){
             for( ClientThread member : channelMembers ){
                 member.send( "MSG " + this.chanelName + " " + msg );
+            }
+        }
+        public void rawSend(String msg ){
+            for( ClientThread member : channelMembers ){
+                member.send( msg );
+            }
+        }
+        public void send( String msg, String from ){
+            for( ClientThread member : channelMembers ){
+                member.send( "MSG " + from + " " + msg );
             }
         }
 
@@ -46,6 +60,48 @@ public class Server {
         serverChannels.put("#default", new Channel("#default") );
     }
 
+    public void handleNick(ClientThread clientThread, String msg) {
+        connections.remove( clientThread );
+        registerUser.add( clientThread );
+        serverChannels.get("#default").addMember( clientThread );
+    }
+
+    public void handleUsers(ClientThread clientThread, String msg) {
+        String result = "[ ";
+        for( ClientThread user : registerUser ){
+            result+=user.getUserName() + " " ;
+        }
+        result+="]";
+        clientThread.send( result );
+    }
+
+    public void handleMsg(ClientThread clientThread, String msg) {
+        String msgTokens[] = msg.split(" ", 3 );    // 0 - token MSG, 1 - target, 2 - content
+        if( msgTokens.length < 3 ){
+            return;
+        }
+        System.out.println( "MSG [" + msgTokens[1] +"][" + msgTokens[2] + "]" );
+        if( msgTokens[1].startsWith("#" ) ){
+            Channel target = serverChannels.get( msgTokens[1] );
+            if( target != null ){
+                //target.send(msgTokens[2], msgTokens[1] );
+                String decoratedMessage = "MSG " + msgTokens[1] +" " + clientThread.getUserName() + " " + msgTokens[2] ;
+                target.rawSend( decoratedMessage );
+            }else{
+                System.out.println( "Channel nie znaleziony" );
+                return;
+            }
+        }else{ //user
+            ClientThread target = findUser( msgTokens[1] );
+            if( target != null ){
+                target.send( msgTokens[2], clientThread.getUserName() );
+            }else{
+                System.out.println( "uzytkownik nie znaleziony" );
+                return;
+            }
+        }
+    }
+
     public void handleJoin(ClientThread clientThread, String msg) {
         String channels[] =msg.split(" " );
         for( String channel : channels ) {
@@ -56,7 +112,7 @@ public class Server {
                     serverChannels.put(channel, ch);
                 }
                 ch.addMember(clientThread);
-                System.out.println( "Addded " + clientThread + " to channel " + channel );
+                System.out.println( "Addded " + clientThread.getUserName() + " to channel " + channel );
             }
         }
     }
@@ -80,8 +136,9 @@ public class Server {
             try {
                 client = server.accept();
                 ClientThread c = new ClientThread(client, this );
-                userts.add(c);
-                serverChannels.get( "#default" ).addMember( c );
+                //registerUser.add(c);
+                //serverChannels.get( "#default" ).addMember( c );
+                connections.add( c );
                 c.start();
             }catch( Exception e ){
                 System.out.println( e.getStackTrace() );
@@ -89,8 +146,18 @@ public class Server {
         }
     }
     public void sendToAll( String msg){
-        for( ClientThread cl : userts ){
+        for( ClientThread cl : registerUser){
               cl.send(msg);
         }
+    }
+    public ClientThread findUser( String name ){
+        for( ClientThread user : registerUser ){
+            String u = user.getUserName();
+
+            if( user.getUserName().equals( name ) ){
+                return user;
+            }
+        }
+        return null;
     }
 }
